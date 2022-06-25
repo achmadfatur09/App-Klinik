63e8e6ffd57dda9fd57823f2dea02e6a2891f99f
Bravo Aro Cardova <bravoarocardova@gmail.com>
2022-06-25 17:10:41 +0700

    fix error hospital

--- src/pages/RsProfile/index.js
+++ src/pages/RsProfile/index.js
@@ -7,38 +7,35 @@ import { DummyHospital1, JSONHospital } from '../../assets';
 
 export default function RsProfile({ navigation , route}) {
   const {id} = route.params;
-  const [Rs, setRS] = useState({});
 
-  useEffect(() => {
-    setRS(JSONHospital.data.filter(i => i.id==id))
-  }, [])
+  const rs =JSONHospital.data.filter(i => i.id==id);
 
   return (
     <ScrollView>
       <View style={styles.page}>
         <Header title="Profile Rumah Sakit" onPress={() => navigation.goBack()} />
         <RumahSakit
-          name={Rs[0].name}
-          desc={Rs[0].type}
+          name={rs[0].name}
+          desc={rs[0].type}
           photo={DummyHospital1}
         />
         <Gap height={10} />
         <ProfileItem
           label="Rumah Sakit Umum"
-          value={Rs[0].type}
+          value={rs[0].type}
         />
         <ProfileItem
           label="Alamat Rumah Sakit"
-          value={Rs[0].address}
+          value={rs[0].address}
         />
         <ProfileItem
           label="No. HP Rumah Sakit"
-          value={Rs[0].phone}
+          value={rs[0].phone}
         />
         <View style={styles.action}>
           <Button title="Call Rumah Sakit"
           // langsung ke arah diall hp
-          onPress={()=>{Linking.openURL('tel:'+ Rs[0].phone)}}
+          onPress={()=>{Linking.openURL('tel:'+ rs[0].phone)}}
           />
         </View>
       </View>
